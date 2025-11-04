458d8a098a3fd72070202e08f744baa35665c642
benjaestupinan <benja.3stupinan@gmail.com>
2025-11-04 10:37:49 -0300

    removed eslint_d, added go and fontforge to .gitignore, changed neo-tree command to toggle

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -8,6 +8,6 @@ return {
   },
   lazy = false, -- neo-tree will lazily load itself
   config = function()
-    vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal toggle<CR>', {})
   end
 }
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -10,7 +10,6 @@ return {
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
 				null_ls.builtins.diagnostics.rubocop,
-				null_ls.builtins.diagnostics.eslint_d,
 			},
 		})
 
