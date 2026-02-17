78533d93a14435fe2f3bcf34f94f6c0c7317fc68
benjaestupinan <benja.3stupinan@gmail.com>
2026-02-17 00:15:48 -0300

    minor changes

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -5,8 +5,8 @@ return {
         local dashboard = require("alpha.themes.dashboard")
         dashboard.section.buttons.val = {
             dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-            dashboard.button("<Cntr-p>", "󰈞  Find file", ":Telescope find_files<CR>"),
-            dashboard.button("<Cntr-Shift-f>", "󰈬  Live grep", ":Telescope live_grep<CR>"),
+            dashboard.button("<Ctrl-p>", "󰈞  Find file", ":Telescope find_files<CR>"),
+            dashboard.button("<Ctrl-Shift-f>", "󰈬  Live grep", ":Telescope live_grep<CR>"),
             dashboard.button("q", "  Quit", ":qa<CR>"),
         }
 
