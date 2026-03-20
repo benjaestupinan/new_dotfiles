c261130fd01e766d94418b6f80280f7b07c23819
benjaestupinan <benja.3stupinan@gmail.com>
2026-03-20 16:01:03 -0300

    nvim config update

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,16 +1,38 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.rubocop,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.diagnostics.rubocop,
-			},
-		})
-	end,
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"jay-babu/mason-null-ls.nvim",
+		},
+		config = function()
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({ bufnr = bufnr })
+							end,
+						})
+					end
+				end,
+			})
+		end,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		opts = {
+			ensure_installed = {},
+			automatic_installation = true,
+			handlers = {},
+		},
+	},
 }
